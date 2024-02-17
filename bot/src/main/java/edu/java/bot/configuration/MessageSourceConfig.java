7b6bc36e7020ad171852983f7caf9380f8de313e
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-02-18 01:16:03 +0700

    Added bean MessageSourceConfig

---
+++ bot/src/main/java/edu/java/bot/configuration/MessageSourceConfig.java
@@ -0,0 +1,21 @@
+package edu.java.bot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    private static final String BASE_NAME = "messages";
+    private static final String ENCODING = "UTF-8";
+
+    @Bean
+    public ResourceBundleMessageSource messageSource(){
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename(BASE_NAME);
+        source.setDefaultEncoding(ENCODING);
+        return source;
+    }
+
+}
